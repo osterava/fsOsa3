887e29e578ef5bf6cedd6aaefe10ca060017edfc
osterava <olli.s.terava@student.jyu.fi>
2023-12-05 14:43:51 +0200

    uibuild

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
+
 const app = express()
 
+app.use(cors())
 app.use(express.json())
+app.use(express.static('dist'))
 
 morgan.token('postData', (req) => JSON.stringify(req.body)); 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :postData'));
@@ -123,6 +127,7 @@ app.get('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
-const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
